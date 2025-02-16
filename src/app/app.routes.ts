22d9f88f66201d60947edc6291cf8d3310318800
misskhoudia <misskhoudiakebe@gmail.com>
2025-02-16 18:11:45 +0000

    /feat mise en place des routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,14 @@
+app.routes.ts
+
 import { Routes } from '@angular/router';
+import { ImportElecteurComponent } from './components/import-electeurs/import-electeurs.component';
+import { GestionCandidatsComponent } from './components/gestion-candidat/gestion-candidat.component';
+import { MonitoringComponent } from './components/monitoring/monitoring.component';
+import { AccueilComponent } from './components/accueil/accueil.component';
 
 export const routes: Routes = [
-
+    { path: '', component: AccueilComponent },
+    { path: 'import-electeurs', component: ImportElecteurComponent },
+    { path: 'gestion-candidats', component: GestionCandidatsComponent },
+    { path: 'monitoring', component: MonitoringComponent },
 ];
